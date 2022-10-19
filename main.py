f6a89452fe750b14f591429e0e5e7f48177e60b4
Aurelia Segarra <av.segarra@gmail.com>
2022-10-19 12:31:11 +0200

    :tada: initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import *
+
+root = Tk()
+root.title("Py Calculator")
+root.geometry("570x600+100+200")
+root.resizable(False, False)
+root.configure(bg="#17161b")
+
+root.mainloop()
